6a06b7e04a5135a33c26ac15dcf5ec9cc7d11668
Robert Field <rfields03@gmail.com>
2022-03-11 10:59:58 -0500

    update SQL

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'gmail.com',
+    // Your MySQL username,
+    user: 'rfields03',
+    // Your MySQL password
+    password: 'Af983426!@',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
